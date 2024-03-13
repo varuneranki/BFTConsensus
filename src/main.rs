85c442dd03e93ca31d8e3725d83de965a269421f
Varun Maitreya Eranki <varun.maitreya@gmail.com>
2024-03-13 18:41:24 +0100

    simulation for 22 servers is done and notes regarding output have been added

--- src/main.rs
+++ src/main.rs
@@ -145,7 +145,7 @@ impl Server {
 // }
 
 fn main() {
-    let num_servers = 8; // Adjust as needed
+    let num_servers = 22; // Adjust as needed
 
     // Create a vector to store servers
     let mut servers: Vec<Server> = Vec::with_capacity(num_servers);
