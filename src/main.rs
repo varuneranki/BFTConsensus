b41750146e49f6eba2463eec8a7a729481cc2070
Varun Maitreya Eranki <varun.maitreya@gmail.com>
2024-03-13 14:49:18 +0100

    Sample code 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+    let mut a = 2;
+    a = a+2;
+    println!("value: {} {}", a , a / 6);
+
+    /*if a % 2 == 0 && a % 1 == a {
+        println!("even");
+    } else {
+        println!("odd");
+    }*/
+
+
 }
